85ce9b54476ccf18a8bb770f3596cb6d92414d56
Renata Rauber <renatarauber77@gmail.com>
2025-10-23 18:14:44 -0300

    alugames

---
+++ app.js
@@ -0,0 +1,35 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+    // confirmaçao para devolver jogo
+    if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosAlugados--;
+    }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return')
+        jogosAlugados++;
+    }
+
+    contarEExibirJogosAlugados();
+}
+// inicializa a contagem considerando que os jogos ja começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
